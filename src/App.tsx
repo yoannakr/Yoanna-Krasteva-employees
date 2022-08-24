d6e8e1fef1a6bd37e26555de0f3d6c0d41fdcaba
yoannakr <jkrmilkova@gmail.com>
2022-08-24 23:33:36 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const uploadFile = (file: File | undefined) => {
+    console.log(file);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={(event) => uploadFile(event?.target?.files?.[0])}
+      />
     </div>
   );
 }
