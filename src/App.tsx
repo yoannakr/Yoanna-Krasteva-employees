54bc470e0b3516e0d0e824f86fa47333824bbd9e
yoannakr <jkrmilkova@gmail.com>
2022-08-26 11:07:35 +0300

    Removed unused imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Papa from "papaparse";
-import { IEmployee } from "./types/employee";
-import { parseEmployeeData } from "./utils/parseEmployeeData";
-import { groupBy } from "./utils/groupBy";
-import { getTotalDaysDifferenceBetweenDates } from "./utils/getTotalDaysDifferenceBetweenDates";
-import { IPair } from "./types/pair";
 import PairEmployeesList from "./components/PairEmployeesList";
 
 function App() {
